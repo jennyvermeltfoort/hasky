d4221ad2e46bc8f55d9d1f5e30aaca4d8a39f572
Jenny Vermeltfoort <jennyvermeltfoort@outlook.com>
2024-12-02 23:21:50 +0100

    sicko

--- src/Main.hs
+++ src/Main.hs
@@ -2,15 +2,15 @@
 --import System.IO (stdout)
 
 import Data.Char
-import Data.List
-import Data.Set
+import Data.List as List
+import Data.Set as Set hiding (cartesianProduct)
 import Debug.Trace
 import Prelude hiding (map)
 
 data World = World
-  { bombCells :: [(Int, Int)],
-    flagCells :: [(Int, Int)],
-    openCells :: [(Int, Int)],
+  { bombCells :: Set.Set (Int, Int),
+    flagCells :: Set.Set (Int, Int),
+    openCells :: Set.Set (Int, Int),
     dead :: Bool
   }
 
@@ -22,38 +22,25 @@ neighborCells :: (Int, Int) -> [(Int, Int)]
 neighborCells (x, y) = [(a, b) | a <- [x -1 .. x + 1], b <- [y -1 .. y + 1], (a, b) /= (x, y), a >= 0, a < width, b >= 0, b < height]
 
 numNeighborBomb :: ((Int, Int) -> Bool) -> (Int, Int) -> Int
-numNeighborBomb isBombCell (x, y) = length (filter isBombCell (neighborCells (x, y)))
-
-openZeroCells :: ((Int, Int) -> Bool) -> ((Int, Int) -> Int) -> (Int, Int) -> [(Int, Int)] -> [(Int, Int)]
-openZeroCells isFlag numNeighborBomb cell visited
-  | isFlag cell = visited
-  | elem cell visited = visited
-  | numNeighborBomb cell /= 0 = cell : visited
-  | otherwise =
-    let newVisited = cell : visited
-        neighbors = neighborCells cell
-     in Prelude.foldl (\acc neighbor -> openZeroCells isFlag numNeighborBomb neighbor acc) newVisited neighbors
-
-openCell :: ((Int, Int) -> Bool) -> ((Int, Int) -> Int) -> (Int, Int) -> [(Int, Int)]
-openCell isFlag numNeighborBomb cell
-  | isFlag cell = []
-  | numNeighborBomb cell == 0 = openZeroCells isFlag numNeighborBomb cell []
-  | otherwise = [cell]
+numNeighborBomb isBombCell (x, y) = length (Prelude.filter isBombCell (neighborCells (x, y)))
 
 baseState :: World
 baseState =
   World
-    { bombCells = [(2, 0), (2, 1), (2, 2)],
-      flagCells = [(1, 1)],
-      openCells = [],
+    { bombCells = Set.fromList [(2, 0)],
+      flagCells = Set.fromList [],
+      openCells = Set.fromList [],
       dead = False
     }
 
 allCells = [(x, y) | x <- [0 .. width - 1], y <- [0 .. height - 1]]
 
-zeroBombCells numNeighborBomb = [x | x <- allCells, numNeighborBomb x == 0]
+zeroBombCells numNeighborBomb isBomb = [x | x <- allCells, numNeighborBomb x == 0, not (isBomb x)]
 
-cellIsNeighbor a b = elem b (neighborCells a)
+neighborCellsBottomRight :: (Int, Int) -> [(Int, Int)]
+neighborCellsBottomRight (x, y) = [(x + 1, y), (x, y + 1)]
+
+cellIsNeighbor a b = elem b (neighborCellsBottomRight a)
 
 cellCreateNeighborRelation a b
   | cellIsNeighbor a b = [(a, b)]
@@ -75,45 +62,31 @@ cellCreateAllNeighborRelations zeroCells
         relations = (cellCreateCellNeighborRelations nextCells firstCell)
      in relations ++ cellCreateAllNeighborRelations nextCells
 
+cartesianProduct :: [((Int, Int), (Int, Int))] -> [((Int, Int), (Int, Int))] -> [((Int, Int), (Int, Int))]
+cartesianProduct a b = [(a0, b1) | (a0, a1) <- a, (b0, b1) <- b]
+
+inverseRelation :: [((Int, Int), (Int, Int))] -> [((Int, Int), (Int, Int))]
+inverseRelation a = [(a1, a0) | (a0, a1) <- a]
+
+makeTransitive :: [((Int, Int), (Int, Int))] -> [((Int, Int), (Int, Int))]
+makeTransitive a =
+  Set.toList (Set.unions [(Set.fromList a), (makeTransitive' a 1), (makeTransitive' a 2)])
+  where
+    makeTransitive' a 0 = Set.fromList (cartesianProduct a a)
+    makeTransitive' a num =
+      let product = Set.fromList (cartesianProduct a a)
+          next = makeTransitive' a (num - 1)
+       in Set.union product next
+
+makeSymmetric :: [((Int, Int), (Int, Int))] -> [((Int, Int), (Int, Int))]
+makeSymmetric a = a ++ inverseRelation a
+
 main :: IO ()
 main =
   do
-    let zeroCells = zeroBombCells (numNeighborBomb (\n -> elem n (bombCells baseState)))
-    print $ cellCreateAllNeighborRelations zeroCells
-
-openState :: World -> (Int, Int) -> World
-openState state cell =
-  let newOpenCells = openCell (\n -> elem n (flagCells baseState)) (numNeighborBomb (\n -> elem n (bombCells baseState))) cell
-      newDead =
-        if dead state
-          then dead state
-          else elem cell (bombCells state)
-   in World
-        { bombCells = bombCells state,
-          flagCells = flagCells state,
-          openCells = openCells state ++ newOpenCells,
-          dead = newDead
-        }
-
-intToChar :: Int -> Char
-intToChar n
-  | n >= 0 && n <= 9 = chr (n + 48) -- 48 is the ASCII code for '0'
-  | otherwise = error "Input must be a single digit (0-9)"
-
-cellChar state cell
-  | elem cell (openCells state) && elem cell (bombCells state) = 'x'
-  | elem cell (openCells state) = intToChar (numNeighborBomb (\n -> elem n (bombCells baseState)) cell)
-  | elem cell (flagCells state) = 'f'
-  | otherwise = '.'
-
-grid state = [cellChar state cell | cell <- allCells]
-
-printGrid (x : grid) index
-  | grid == [] = putChar x
-  | index < width = do
-    putChar x
-    printGrid grid (index + 1)
-  | otherwise = do
-    putChar '\n'
-    putChar x
-    printGrid grid 1
+    let isBomb = (\n -> member n (bombCells baseState))
+    let zeroCells = zeroBombCells (numNeighborBomb isBomb) isBomb
+    let relations = cellCreateAllNeighborRelations zeroCells
+    let symmetric = makeSymmetric relations
+    let paths = Set.fromList (makeTransitive symmetric)
+    print $ List.groupBy (\a b -> member (a, b) paths) zeroCells
